e2728fc0fef3d0dbc92b7536154c85d6bfddf4ea
Michael Gene Brockus <55331536+michael-gene-brockus@users.noreply.github.com>
2022-05-25 00:19:50 -0700

    Update test_cases.cpp

--- test/test_cases.cpp
+++ test/test_cases.cpp
@@ -4,57 +4,23 @@
 // mail: <mailto: michaelbrockus@gmail.com>
 //
 #include "troglobyte/package.hpp"
-#include <unity.h>
-
-
-//
-//  project setup teardown functions if needed
-//
-void setUp(void)
-{
-    // TODO.
-} // end of function setUp
-
-void tearDown(void)
-{
-    // TODO.
-} // end of function tearDown
+#include <gtest/gtest.h>
 
 //
 // list of all the test cases for this project
 //
-static void test_simpleAssertTrue()
+TEST(simpleAssertTrue)
 {
-    TEST_ASSERT_TRUE(true);
+    ASSERT_TRUE(true);
 } // end of test case
 
-static void test_simpleAssertNull()
-{
-    TEST_ASSERT_NULL(NULL);
-} // end of test case
-
-static void test_simpleAssertCompare()
+TEST(simpleAssertCompare)
 {
     auto dummy = 3;
-    TEST_ASSERT_EQUAL_INT(3, dummy);
+    ASSERT_EQ(3, dummy);
 } // end of test case
 
-static void test_simpleAssertCall()
+TEST(simpleAssertCall)
 {
-    TEST_ASSERT_EQUAL_STRING("Hello, C++ Developer.", trog::greet());
+    ASSERT_EQ("Hello, C++ Developer.", trog::greet());
 } // end of test case
-
-//
-//  here main is used as the test runner
-//
-auto main() -> int
-{
-    UNITY_BEGIN();
-
-    RUN_TEST(test_simpleAssertTrue);
-    RUN_TEST(test_simpleAssertNull);
-    RUN_TEST(test_simpleAssertCall);
-    RUN_TEST(test_simpleAssertCompare);
-
-    return UNITY_END();
-} // end of func
