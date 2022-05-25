359cb559f63c1b47c0bf01e9f0d5cbda92328bb2
Michael Gene Brockus <55331536+michael-gene-brockus@users.noreply.github.com>
2022-05-25 10:10:12 -0700

    Update test_cases.cpp

--- test/test_cases.cpp
+++ test/test_cases.cpp
@@ -9,18 +9,18 @@
 //
 // list of all the test cases for this project
 //
-TEST(simpleAssertTrue)
+TEST(testFixture, simpleAssertTrue)
 {
     ASSERT_TRUE(true);
 } // end of test case
 
-TEST(simpleAssertCompare)
+TEST(testFixture, simpleAssertCompare)
 {
     auto dummy = 3;
     ASSERT_EQ(3, dummy);
 } // end of test case
 
-TEST(simpleAssertCall)
+TEST(testFixture, simpleAssertCall)
 {
     ASSERT_EQ("Hello, C++ Developer.", trog::greet());
 } // end of test case
